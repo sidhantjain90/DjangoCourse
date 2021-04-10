f4d3e0aae215d414fece6d28399272936472d472
sidhant <sidhant.jain90@gmail.com>
2021-04-10 19:49:18 +0200

    Python Lvl 1 Exercise

---
+++ strings.py
@@ -0,0 +1,17 @@
+# Strings
+
+myString = 'abcdefg'
+print(myString[2:])
+
+print(myString[:4])
+
+print(myString[::2])
+
+#String methods
+
+x = myString.split('d')
+print(x)
+
+#Print formatting
+x = "Insert another string here: {}".format("Insert ME!")
+print(x)
